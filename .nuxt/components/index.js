c7a57bad242bbed14a52e0a3e61c1ce2f7662403
rovh <56371012+rovh@users.noreply.github.com>
2022-02-05 05:39:23 +0300

    private

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,9 +1,9 @@
-export const CarouselComponent = () => import('../..\\components\\CarouselComponent.vue' /* webpackChunkName: "components/carousel-component" */).then(c => wrapFunctional(c.default || c))
-export const CommentFormComponent = () => import('../..\\components\\CommentFormComponent.vue' /* webpackChunkName: "components/comment-form-component" */).then(c => wrapFunctional(c.default || c))
-export const CommentListComponent = () => import('../..\\components\\CommentListComponent.vue' /* webpackChunkName: "components/comment-list-component" */).then(c => wrapFunctional(c.default || c))
-export const CommentsComponent = () => import('../..\\components\\CommentsComponent.vue' /* webpackChunkName: "components/comments-component" */).then(c => wrapFunctional(c.default || c))
-export const FooterComponent = () => import('../..\\components\\FooterComponent.vue' /* webpackChunkName: "components/footer-component" */).then(c => wrapFunctional(c.default || c))
-export const HeaderComponent = () => import('../..\\components\\HeaderComponent.vue' /* webpackChunkName: "components/header-component" */).then(c => wrapFunctional(c.default || c))
+export { default as CarouselComponent } from '../..\\components\\CarouselComponent.vue'
+export { default as CommentFormComponent } from '../..\\components\\CommentFormComponent.vue'
+export { default as CommentListComponent } from '../..\\components\\CommentListComponent.vue'
+export { default as CommentsComponent } from '../..\\components\\CommentsComponent.vue'
+export { default as FooterComponent } from '../..\\components\\FooterComponent.vue'
+export { default as HeaderComponent } from '../..\\components\\HeaderComponent.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
